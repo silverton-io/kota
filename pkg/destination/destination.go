6f20812cc009a14ce66db86036a4c9683fd23170
jakthom <aerialfly@gmail.com>
2024-03-30 13:14:24 -0400

    envelope and sink interface

---
+++ pkg/destination/destination.go
@@ -0,0 +1,16 @@
+// Copyright (c) 2024 Silverton Data, Inc.
+// You may use, distribute, and modify this code under the terms of the Apache-2.0 license, a copy of
+// which may be found at https://github.com/silverton-io/kota/blob/main/LICENSE
+
+package destination
+
+import (
+	"github.com/silverton.io/kota/pkg/config"
+	"github.com/silverton.io/kota/pkg/envelope"
+)
+
+type Destination interface {
+	Metadata() map[string]string // FIXME - don't use map[string]interface
+	Initialize(config config.Sink) error
+	Send(batch []envelope.KotaEnvelope) error
+}
