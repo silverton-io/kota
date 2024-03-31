2bbe4ad1f19fa524d8d38493fb99207e2387c700
jakthom <aerialfly@gmail.com>
2024-03-31 10:54:22 -0400

    Set up record count-based buffer purging

--- pkg/buffer/buffer.go
+++ pkg/buffer/buffer.go
@@ -5,6 +5,8 @@
 package buffer
 
 import (
+	"time"
+
 	"github.com/rs/zerolog/log"
 	"github.com/silverton.io/kota/pkg/config"
 	"github.com/silverton.io/kota/pkg/envelope"
@@ -12,21 +14,29 @@ import (
 )
 
 type Buffer struct {
-	config    *config.Config
-	inputChan chan envelope.KotaEnvelope
-	envelopes []envelope.KotaEnvelope
-	shutdown  chan int
+	config        *config.Config
+	bufferRecords int
+	bufferStart   time.Time
+	inputChan     chan envelope.KotaEnvelope
+	envelopes     []envelope.KotaEnvelope
+	shutdown      chan int
 }
 
 func (b *Buffer) Initialize(config *config.Config) error {
 	b.inputChan = make(chan envelope.KotaEnvelope, 20000) // TODO -> will this overflow if the persistence to disk is synchronous? Should it be larger? Smaller? idk, think about this later.
 	b.shutdown = make(chan int, 1)
 	b.config = config
+	b.bufferStart = time.Now()
 	go func(envelope <-chan envelope.KotaEnvelope, shutdown <-chan int) {
 		for {
 			select {
 			case envelope := <-envelope:
-				util.Pprint(envelope) // TODO -> persist to disk, flush, etc
+				b.envelopes = append(b.envelopes, envelope) // Fully rewriting on each append is nawwwwt ideal. FIXME.
+				b.bufferRecords += 1
+				if b.bufferRecords >= b.config.Records { //
+					b.Purge()
+				}
+				util.Pprint(b.envelopes) // TODO -> persist to disk, flush, etc
 			case <-shutdown:
 				log.Debug().Msg("shutting down buffer")
 				// TODO -> do something that is safe on shutdown
@@ -40,7 +50,7 @@ func (b *Buffer) Initialize(config *config.Config) error {
 
 func (b *Buffer) Append(envelopes []envelope.KotaEnvelope) error {
 	for _, envelope := range envelopes {
-		log.Debug().Interface("envelope", envelope).Msg("enqueueing envelope")
+		log.Debug().Msg("appending envelope to buffer")
 		b.inputChan <- envelope
 	}
 	return nil
@@ -48,7 +58,10 @@ func (b *Buffer) Append(envelopes []envelope.KotaEnvelope) error {
 
 func (b *Buffer) Purge() error {
 	log.Debug().Msg("purging buffer")
+	// Actually do something here.
 	b.envelopes = []envelope.KotaEnvelope{}
+	b.bufferStart = time.Now()
+	b.bufferRecords = 0
 	return nil
 }
 
